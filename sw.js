64911ea891f778da70c505ae293ba5bccca1d94c
Axelwickm <axelwickm@gmail.com>
2025-11-19 00:07:30 +0000

    Move service worker path

--- sw.js
+++ sw.js
@@ -4,8 +4,9 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key))),
-    ).then(() => self.clients.claim()),
+    caches
+      .keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.clients.claim()),
   );
 });
